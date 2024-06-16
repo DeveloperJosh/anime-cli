f87b46d78e4a9bd54bdbf82b0c72ae0afec340fa
DeveloperJosh <bam0909@outlook.com>
2024-06-16 15:05:00 -0400

    smol thing

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ const { Command } = require('commander');
 const watchAnime = require('./commands/watch');
 
 const program = new Command();
-program.version('1.0.0');
+program
+    .name('anime-cli')
+    .version('1.0.0')
+    .description('Anime streaming CLI by DeveloperJosh');
 
 program
     .command('watch')
-    .description('Search for an anime and watch an episode')
+    .description('This command will allow you to watch anime from the command line.')
     .action(watchAnime);
 
 program.parse(process.argv);
