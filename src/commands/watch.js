1d7cf6555d9a272c6a889944c440de04daa2ed5b
DeveloperJosh <bam0909@outlook.com>
2024-06-25 01:58:28 -0400

    chore: Fix animeNameSlug replacement in showAnimeInfo function

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -393,7 +393,7 @@ async function showAnimeInfo(animeName) {
     .replace(/-+$/, '');
 
     const infoUrl = `${config.api}/api/info/${animeNameSlug}`;    
-    fs.writeFileSync('test.json', JSON.stringify(infoUrl));
+    //fs.writeFileSync('test.json', JSON.stringify(infoUrl));
     const infoResponse = await axios.get(infoUrl);
     let animeInfo = infoResponse.data;
     let text = `Title: ${animeInfo.title}\nTotal Episodes: ${animeInfo.totalEpisodes}\nGenres: ${animeInfo.genres.join(', ')}\nStatus: ${animeInfo.status}\nRelease Date: ${animeInfo.released}\nDescription: ${animeInfo.description}`;
